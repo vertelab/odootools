282bf1981f8ec55de3ea463cb028f83c08fdef11
aswanth-confianz <aswanth.babu@confianzit.biz>
2021-10-29 10:21:26 +0530

    [UPDATE-FIX] multi host, port, fixed postgres subprocess issue fixed

--- odoobackup.py
+++ odoobackup.py
@@ -2,16 +2,24 @@
 import odoo
 import click
 
+
 @click.command()
 @click.option('--database', prompt='database', help='Database to backup.')
 @click.option('--filename', prompt='filename', help='File Name to save the Backup.')
-def backup(database,filename):
-    backup = odoo.service.db.dump_db(database,None)
-    with open(filename,'wb') as f:
+@click.option('--host', prompt='host', help='Database Host name.')
+@click.option('--db_port', prompt='db_port', help='Database working Port.')
+@click.option('--db_user', prompt='db_user', help='Database User.')
+@click.option('--db_password', prompt='db_password', help='Database Password.')
+@click.option('--stream', prompt='stream', help='if stream is None return a file object with the dump')
+def backup(database, filename):
+    odoo.tools.config['db_port'] = db_port
+    odoo.tools.config['db_host'] = host
+    odoo.tools.config['db_user'] = db_user
+    odoo.tools.config['db_password'] = db_password
+    backup = odoo.service.db.dump_db(database, stream)
+    with open(filename, 'wb') as f:
         f.write(backup.read())
     backup.close()
     
 if __name__ == '__main__':
     backup()
-
- 
