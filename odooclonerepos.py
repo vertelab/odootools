1a16509b6b5e80b7ef9dd14ab59e577aa9521538
Marcus Jakobsson <84468251+MarkusJacobsson@users.noreply.github.com>
2024-05-06 14:22:39 +0200

    Update odooclonerepos.py
    Added a authtoken.

--- odooclonerepos.py
+++ odooclonerepos.py
@@ -1,6 +1,6 @@
 #!/bin/python3
 import re
-from github import Github
+from github import Github, Auth
 import requests
 
 import sys, getopt, os
@@ -18,6 +18,18 @@ def usage():
 GITHUB_BASE_URL = 'https://api.github.com'
 GITHUB_RAW_URL = 'https://raw.githubusercontent.com'
 
+
+
+if not os.access('/etc/odoo/github_auth',os.F_OK):
+    print(f"ERROR we need a file called /etc/odoo/github_auth. Create token in github. To do that go into settings and after that developer settings. Copy the token into github_auth")
+    exit(2)
+
+file = open("/etc/odoo/github_auth")
+content = file.readlines()
+print(content[0].strip())
+
+auth = Auth.Token(content[0].strip())
+g = Github(auth=auth)
 
 try:
     opts, args = getopt.getopt(sys.argv[1:], "r:b:la", ["repo=", "branch=", "list", "all",])
@@ -61,7 +73,6 @@ GITHUB_RAW_URL = 'https://raw.githubusercontent.com'
 
 def clone_repo(repo,branch):
     
-    g = Github()
 
     if os.access('/usr/share',os.F_OK):
         if not os.access('/usr/share',os.W_OK):
@@ -77,9 +88,11 @@ def clone_repo(repo,branch):
         print(f"Error {response.status_code}  {repo_url}")
     
     for gitrepo in response.text.split('\n'):
-        print(gitrepo)
-        author = repo.split('-')[0]
-        repobase = repo.split('-')[1].split('.')[0]
+        if not gitrepo:
+            continue
+        author = gitrepo.split('/')[0]
+        repobase = gitrepo.split('/')[1].split('.')[0]
+        
         if author == 'vertelab':
             target = f"/usr/share/{repobase}"
         else:
@@ -97,7 +110,8 @@ def clone_repo(repo,branch):
         
         branches = [b.name for b in grepo.get_branches() if re.match("^\d*[.]0$", b.name)]
         if not BRANCH in branches:
-            print(f"Branch  is missing {author}/{repobase}")
+            print(f"Branch ({BRANCH}) is missing {author}/{repobase}")
+            continue
         # ~ os.system(f"git clone -b {BRANCH} git@github.com:{gitrepo} > /dev/null 2> /dev/null")
         os.system(f"git clone -b {BRANCH} git@github.com:{gitrepo}")
         if os.access(repobase,os.F_OK):
@@ -110,17 +124,17 @@ def clone_repo(repo,branch):
 
 
 if LIST:
-    g = Github()
     repo = g.get_repo(f"vertelab/odootools")
-
     repos = [c.name for c in repo.get_contents("repos") if re.match(".*-repo$",c.name)]
     print(','.join(repos))
 elif REPO:
     clone_repo(REPO,BRANCH)
 elif ALL:
-    g = Github()
     repo = g.get_repo(f"vertelab/odootools")
+    print(f'{repo.get_contents("repos/thespino")=}')
+    
 
     for repo in [c.name for c in repo.get_contents("repos") if re.match(".*-repo$",c.name)]:
+        #print(repo)
         clone_repo(repo,BRANCH)
 
