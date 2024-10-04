2268c5935f3ff8d315b286505ebd8e79b381dcfd
marcuszxc <115086076+marcuszxc@users.noreply.github.com>
2024-10-04 09:52:38 +0200

    odootools.sh: Wrong path for core-odoo

--- odootools.sh
+++ odootools.sh
@@ -466,7 +466,7 @@ function odooinstallocb() {
     sudo pip install -U git+https://github.com/OCA/OCB.git@"$VERSION" 
 
     echo create symlinks to cores
-    sudo ln -s /usr/local/lib/python3.8/dist-packages/odoo/addons /usr/share/core-odoo
+    sudo ln -s /usr/local/lib/python3.8/dist-packages/odoo /usr/share/core-odoo
     sudo ln -s /usr/src/OCB/addons /usr/share/core-ocb
 
     ADDONS_PATH=$(sudo grep "addons_path" $ODOO_CONFIGURATION_FILE)
