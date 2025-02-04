dffc283838c0ff0297a2b54d6575de4e68126ee3
marcuszxc <115086076+marcuszxc@users.noreply.github.com>
2025-02-04 08:33:28 +0100

    Update odootools.sh

--- odootools.sh
+++ odootools.sh
@@ -637,7 +637,7 @@ function _odoobranchdiff() {
 		git diff --quiet "$SBRANCH" "$DBRANCH" -- "$file" && continue
 		echo  "${file}".diff
 		git diff -U10000 "$SBRANCH" "$DBRANCH" -- "$file" > "${file}".diff
-                perl -pi -e 's/\s//' "${file}".diff 
+                perl -pi -e 's/^\s//' "${file}".diff 
 	done
 	git checkout "$OBRANCH"
 	cd "$CWD"
