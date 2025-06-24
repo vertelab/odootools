8997169b6f5ee715e0a6c414b825bef9a5765984
marcuszxc <115086076+marcuszxc@users.noreply.github.com>
2025-06-24 13:49:05 +0200

    Update create_fake_addons.sh

--- create_fake_addons.sh
+++ create_fake_addons.sh
@@ -95,7 +95,7 @@ for project in "${ADDONS_PROJECTS[@]}"; do
         fi
 
         # Copy and empty XML files in views and data
-        for subdir in views data; do
+        for subdir in views view data templates snippets demo; do
             if [ -d "$module/$subdir" ]; then
                 mkdir -p "$dest/$subdir"
                 for xml in "$module/$subdir"/*.xml; do
@@ -105,58 +105,6 @@ for project in "${ADDONS_PROJECTS[@]}"; do
                     [ -f "$csv" ] && echo "id,name" > "$dest/$subdir/$(basename "$csv")"
                 done
 
-            fi
-        done
-
-        # Copy and empty XML files in views and data
-        for subdir in view data; do
-            if [ -d "$module/$subdir" ]; then
-                mkdir -p "$dest/$subdir"
-                for xml in "$module/$subdir"/*.xml; do
-                    [ -f "$xml" ] && echo "<odoo></odoo>" > "$dest/$subdir/$(basename "$xml")"
-                done
-                for csv in "$module/$subdir"/*.csv; do
-                    [ -f "$csv" ] && echo "id,name" > "$dest/$subdir/$(basename "$csv")"
-                done
-            fi
-        done
-
-        # Copy and empty XML files in templates and data
-        for subdir in templates data; do
-            if [ -d "$module/$subdir" ]; then
-                mkdir -p "$dest/$subdir"
-                for xml in "$module/$subdir"/*.xml; do
-                    [ -f "$xml" ] && echo "<odoo></odoo>" > "$dest/$subdir/$(basename "$xml")"
-                done
-                for csv in "$module/$subdir"/*.csv; do
-                    [ -f "$csv" ] && echo "id,name" > "$dest/$subdir/$(basename "$csv")"
-                done
-            fi
-        done
-
-        # Copy and empty XML files in snippets and data
-        for subdir in snippets data; do
-            if [ -d "$module/$subdir" ]; then
-                mkdir -p "$dest/$subdir"
-                for xml in "$module/$subdir"/*.xml; do
-                    [ -f "$xml" ] && echo "<odoo></odoo>" > "$dest/$subdir/$(basename "$xml")"
-                done
-                for csv in "$module/$subdir"/*.csv; do
-                    [ -f "$csv" ] && echo "id,name" > "$dest/$subdir/$(basename "$csv")"
-                done
-            fi
-        done
-    
-        # Copy and empty XML files in snippets in views and data
-        for subdir in snippets data; do
-            if [ -d "$module/views/$subdir" ]; then
-                mkdir -p "$dest/views/$subdir"
-                for xml in "$module/views/$subdir"/*.xml; do
-                    [ -f "$xml" ] && echo "<odoo></odoo>" > "$dest/views/$subdir/$(basename "$xml")"
-                done
-                for csv in "$module/views/$subdir"/*.csv; do
-                    [ -f "$csv" ] && echo "id,name" > "$dest/views/$subdir/$(basename "$csv")"
-                done
             fi
         done
 
