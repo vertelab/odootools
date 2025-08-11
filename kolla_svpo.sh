d80c51a646e8e25020b6a04e80dfb9f3bbc61bbc
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2025-08-11 13:58:42 +0200

    T/5088 Create kolla_svpo.sh

---
+++ kolla_svpo.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# 
+# Sökvägens mönster
+BASE_PATH="/usr/share/odoo-*/*"
+
+echo "Vertel: Letar efter kataloger där 'sv.po' saknas..."
+
+# Leta igenom alla matchande kataloger
+for dir in $BASE_PATH; do
+    # Kontrollera om det är en katalog
+    if [ -d "$dir" ]; then
+        if [ ! -f "$dir/i18n/sv.po" ]; then
+            echo "Saknas: $dir"
+        fi
+    fi
+done
+
+echo " "
+echo "..."
+echo " "
+
+# Sökvägens mönster
+BASE_PATH="/usr/share/odooext-OCA*/*"
+
+echo "OCA: Letar efter kataloger där 'sv.po' saknas..."
+
+# Leta igenom alla matchande kataloger
+for dir in $BASE_PATH; do
+    # Kontrollera om det är en katalog
+    if [ -d "$dir" ]; then
+        if [ ! -f "$dir/i18n/sv.po" ]; then
+            echo "Saknas: $dir"
+        fi
+    fi
+done
+
