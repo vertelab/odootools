1a2a7f99c98d7f757b4d85f6644558ae8585a944
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2025-08-11 14:09:22 +0200

    T/5088 Update kolla_svpo.sh

--- kolla_svpo.sh
+++ kolla_svpo.sh
@@ -9,7 +9,7 @@ echo "Vertel: Letar efter kataloger där 'sv.po' saknas..."
 for dir in $BASE_PATH; do
     # Kontrollera om det är en katalog
     if [ -d "$dir" ]; then
-        if [ ! -f "$dir/i18n/sv.po" ]; then
+        if [ ! -f "$dir/i18n/sv.po" ] && [ ! -f "$dir/i18n/sv_SE.po" ]; then
             echo "Saknas: $dir"
         fi
     fi
@@ -28,7 +28,7 @@ echo "OCA: Letar efter kataloger där 'sv.po' saknas..."
 for dir in $BASE_PATH; do
     # Kontrollera om det är en katalog
     if [ -d "$dir" ]; then
-        if [ ! -f "$dir/i18n/sv.po" ]; then
+        if [ ! -f "$dir/i18n/sv.po" ] && [ ! -f "$dir/i18n/sv_SE.po" ]; then
             echo "Saknas: $dir"
         fi
     fi
