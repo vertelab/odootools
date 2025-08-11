81f618aa7f88618903f5b3aa460b9b9c30731a1c
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2025-08-11 16:47:10 +0200

    T/5088 Update kolla_svpo.sh

--- kolla_svpo.sh
+++ kolla_svpo.sh
@@ -21,16 +21,26 @@ echo " "
 
 # Sökvägens mönster
 BASE_PATH="/usr/share/odooext-OCA*/*"
-
-echo "OCA: Letar efter kataloger där 'sv.po' saknas..."
-
 # Leta igenom alla matchande kataloger
 for dir in $BASE_PATH; do
+
+
     # Kontrollera om det är en katalog
     if [ -d "$dir" ]; then
         if [ ! -f "$dir/i18n/sv.po" ] && [ ! -f "$dir/i18n/sv_SE.po" ]; then
-            echo "Saknas: $dir"
+
+           # Extrahera modulnamn (sista delen i sökvägen)
+           module_name=$(basename "$dir")
+
+           # Extrahera modulbasnamn efter "odooext-OCA-"
+           OCA_path=$(echo "$dir" | sed -E 's#.*/odooext-OCA-([^/]+)/.*#\1#')
+           echo "Saknas: $dir"
+           # Bygg URL
+           OCA_URL="https://translation.odoo-community.org/projects/${OCA_path}-${ODOO_VERSION}-0/${OCA_path}-${ODOO_VERSION}-0-${module_name}/"
+           echo "$OCA_URL"
+           echo ""
         fi
     fi
 done
+
 
