086f91cd736c40a55f625ae852c56c569e034ff3
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2025-08-12 09:16:56 +0200

    T/5088 Create kolla_svpo.sh >> moving to common/scripts branch

---
+++ scripts/kolla_svpo.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+# 
+# Sökvägens mönster
+BASE_PATH="/usr/share/odoo-*/*"
+
+echo "Vertel: Letar efter kataloger där 'sv.po' / 'sv_SE.po' saknas..."
+
+# Leta igenom alla matchande kataloger
+for dir in $BASE_PATH; do
+    # Kontrollera om det är en katalog
+    if [ -d "$dir" ]; then
+        if [ ! -f "$dir/i18n/sv.po" ] && [ ! -f "$dir/i18n/sv_SE.po" ]; then
+            echo "Saknas: $dir"
+        fi
+    fi
+done
+
+echo " "
+echo "..."
+echo " "
+
+# Sökvägens mönster
+BASE_PATH="/usr/share/odooext-OCA*/*"
+# Leta igenom alla matchande kataloger
+for dir in $BASE_PATH; do
+
+    # Kontrollera om det är en katalog
+    if [ -d "$dir" ]; then
+        if [ ! -f "$dir/i18n/sv.po" ] && [ ! -f "$dir/i18n/sv_SE.po" ]; then
+
+           # Extrahera modulnamn (sista delen i sökvägen)
+           module_name=$(basename "$dir")
+
+           # Extrahera modulbasnamn efter "odooext-OCA-"
+           OCA_path=$(echo "$dir" | sed -E 's#.*/odooext-OCA-([^/]+)/.*#\1#')
+           echo "Saknas: $dir"
+           # Bygg URL
+           OCA_URL="https://translation.odoo-community.org/projects/${OCA_path}-${ODOO_VERSION}-0/${OCA_path}-${ODOO_VERSION}-0-${module_name}/"
+           echo "$OCA_URL"
+           echo ""
+        fi
+    fi
+done
+
+
