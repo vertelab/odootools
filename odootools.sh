e00f253899ad36d103d920897151d7d01a73ae46
marcuszxc <marcuszxc10@gmail.com>
2026-01-09 13:34:17 +0100

    odootools.sh: Tryed with exit insted

--- odootools.sh
+++ odootools.sh
@@ -842,7 +842,7 @@ function _odoodisabledb() {
     if [[ -z "$DB_NAME" ]]; then
         echo "Error: Database name (-d) is required" 1>&2
         usage
-        return 1
+        exit 1
     fi
 
     sudo su postgres -c 'psql  -d postgres -c "ALTER DATABASE '$DB_NAME' OWNER TO postgres;"'
@@ -868,7 +868,7 @@ function _odooenabledb() {
     if [[ -z "$DB_NAME" ]]; then
         echo "Error: Database name (-d) is required" 1>&2
         usage
-        return 1
+        exit 1
     fi
 
     sudo su postgres -c 'psql  -d postgres -c "ALTER DATABASE '$DB_NAME' OWNER TO odoo;"'
